7b21da9e9d9b0ae0a86899218f7126b1ac630b3d
Lucas Felipe Lima Cid <lucasfelipaa@gmail.com>
2023-11-08 12:16:44 -0300

    display clock

--- src/pages/menu/index.jsx
+++ src/pages/menu/index.jsx
@@ -1,4 +1,4 @@
-import {Container } from './style'
+import {Container , Calendar} from './style'
 import {Header} from '../../components/header'
 import { SideNav } from '../../components/sideNavMenu'
 import logoImg from '../../assets/photo1697480913-removebg-preview 1.png'
@@ -8,30 +8,111 @@ export const Menu = ()=>{
     const { user } = useAuth()
     const [hours, setHours] = useState()
     const [minutes, setMinutes] = useState()
+    const [displayMins, setDisplay] = useState()
+    const [weekDay, setWeekDay] = useState()
+    const [month, setMonth] = useState()
+    const [day, setDay] = useState()
+    const [year, setYear] = useState()
     const [infoDate, setInfoDate] = useState({})
-    
-    setInterval(() => {
-        const date = new Date()
-        const tempo = {
-            minutos : date.getMinutes(), 
-            horas :  date.getHours()
+    const [nomenclatura, setNomenclatura] = useState({})
+    const [nomDay, setNomDay] = useState({})
+    function separeteNamesOfDates(){
+        console.log(weekDay)
+        switch(weekDay){
+            case 0 :setNomDay({weekDay : 'Domingo'})
+            break;
+            case 1: setNomDay({weekDay : 'Segunda-feira'})
+            break;
+            case 2: setNomDay({weekDay : 'Terça-feira'})
+            break;
+            case 3: setNomDay({weekDay : 'Quarta-feira'})
+            break;
+            case 4: setNomDay({weekDay : 'Quinta-feira'})
+            break;
+            case 5: setNomDay({weekDay : 'Sexta-feira'})
+            break;
+            case 6: setNomDay({weekDay : 'Sabado'})
+            break;
+        }
+        switch(month) {
+            case 0 : setNomenclatura({month : "Janeiro"})
+            break;
+            case 1 : setNomenclatura({month : "Fevereiro"})
+            break;
+            case 2 : setNomenclatura({month : "Março"})
+            break;
+            case 3 : setNomenclatura({month : "Abril"})
+            break;
+            case 4 : setNomenclatura({month : "Maio"})
+            break;
+            case 5 : setNomenclatura({month : "Junho"})
+            break;
+            case 6 : setNomenclatura({month : "Julho"})
+            break;
+            case 7 : setNomenclatura({month : "Agosto"})
+            break;
+            case 8 : setNomenclatura({month : "Setembro"})
+            break;
+            case 9 : setNomenclatura({month : "Outubro"})
+            break;
+            case 10 : setNomenclatura({month : "Novembro"})
+            break;
+            case 11 : setNomenclatura({month : "Dezembro"})
         }
         
-        setHours(tempo.horas)
-        setMinutes(tempo.minutos)
-    }, 1000);
+    }
+    const date = new Date()
+    const tempo = {
+        minutes :  date.getMinutes(), 
+        hours :  date.getHours(),
+        weekDay : date.getDay(),
+        month : date.getMonth() -1,
+        year : date.getFullYear(),
+        day : date.getDate()
+    }
+    setTimeout(() => {
+        
+        setWeekDay(tempo.weekDay)
+
+        setMonth(Number(tempo.month) + 1)
+        setDay(tempo.day)
+        setYear(tempo.year)
+        setHours(()=> {
+            if(Number(tempo.hours) < 10)return `0${tempo.hours}`
+            return tempo.hours 
+        } )
+        setMinutes(()=> {
+            if(Number(tempo.minutes) < 10)return `0${tempo.minutes}`
+            return tempo.minutes
+        } )
+        separeteNamesOfDates()
+        
+        
+    }, 1);
+    useEffect(()=> {
+        setDisplay(minutes)
+    }, [minutes])
     return (
     <Container>
         <Header />
         <SideNav/>
         <img src={logoImg} alt="logo da empresa" />
         <div className="timing">
-            
-            <span>{hours}</span>
-            <span>:</span>
-            <span>{minutes}</span>
+            <div className="clock">
+                <span>{hours}</span>
+                <span>:</span>
+                <span>{displayMins}</span>
 
-        </div>*/
+            </div>
+            <Calendar>
+                <span>{nomDay.weekDay}. </span> 
+                <span>{tempo.day}</span>
+                <span>de</span>
+                <span>{nomenclatura.month}</span>
+                <span>de</span>
+                <span>{tempo.year}</span>
+            </Calendar>
+        </div>
     </Container>
 
     )
--- src/pages/menu/style.js
+++ src/pages/menu/style.js
@@ -8,9 +8,18 @@ export const Container = styled.div`
     "main"
     
     ;
+
     width: 100%;
     height: 100vh;
-    
+    position: relative;
+    .timing {
+        position: absolute;
+        right: 12rem;
+        top: 12rem;
+        .clock{
+            font-size: 2.2rem;
+        }
+    }
     >img { 
         position: absolute;
         top: 20rem;
@@ -19,4 +28,6 @@ export const Container = styled.div`
         margin: 0 auto;
      
     }
+`
+export const Calendar = styled.div`
 `
